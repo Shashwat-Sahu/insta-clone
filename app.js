8fadb426dd46cc79f395729e8c22005ffcce57ce
shashwat Sahu <shashwatsahu25@gmail.com>
2020-12-20 13:02:46 +0530

    server setup

--- app.js
+++ app.js
@@ -34,5 +34,5 @@ mongoose.connection.on("error",(err)=>{
 })
 
 app.listen(PORT,()=>{
-    console.log("Server is running on port : ",5000);
+    console.log("Server is running on port : ",PORT);
 })
