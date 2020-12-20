f2227e7673411c7bf71574649e40d06cf81d8466
shashwat Sahu <shashwatsahu25@gmail.com>
2020-12-20 13:54:21 +0530

    cors changes

--- app.js
+++ app.js
@@ -21,6 +21,16 @@ app.use(require("./routes/user"))
 //         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
 //     })
 // }
+app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+  });
 
 mongoose.connect(MONGOURI,{
     useNewUrlParser: true,
